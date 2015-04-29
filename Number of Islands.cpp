84871b0d71271499f3d5389b7c72d7c34cbf893f
Bread <chisbread@gmail.com>
2015-04-29 13:01:10 +0800

    Create Number of Islands.cpp

---
+++ Number of Islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+	int numIslands(vector<vector<char> > &grid);
+	void search(vector<vector<char> > &grid, int i, int j);
+};
+int Solution::numIslands(vector<vector<char> > &grid) {
+	int num = 0;
+	for(int i = 0; i < grid.size(); ++i) {
+		for(int j = 0; j < grid[i].size(); ++j) {
+			if(grid[i][j] == '1') {
+				search(grid, i, j);
+				++num;
+			}
+		}
+	}
+	return num;
+}
+void Solution::search(vector<vector<char> > &grid, int i, int j) {
+	if(i >= 0 && j >= 0 && i < grid.size() && j < grid[i].size() && (grid[i][j] == '1')) {
+		grid[i][j] = '2';
+		search(grid, i + 1, j);
+		search(grid, i - 1, j);
+		search(grid, i, j + 1);
+		search(grid, i, j - 1);
+	}
+}
