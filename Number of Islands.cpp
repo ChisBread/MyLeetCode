7753d4b87d6c805a13cb9679459737d9a1bc0d88
Bread <chisbread@gmail.com>
2015-04-29 13:12:18 +0800

    Update Number of Islands.cpp
    Note

--- Number of Islands.cpp
+++ Number of Islands.cpp
@@ -8,7 +8,7 @@ int Solution::numIslands(vector<vector<char> > &grid) {
 	for(int i = 0; i < grid.size(); ++i) {
 		for(int j = 0; j < grid[i].size(); ++j) {
 			if(grid[i][j] == '1') {
-				search(grid, i, j);
+				search(grid, i, j);//Delete this island
 				++num;
 			}
 		}
@@ -17,10 +17,11 @@ int Solution::numIslands(vector<vector<char> > &grid) {
 }
 void Solution::search(vector<vector<char> > &grid, int i, int j) {
 	if(i >= 0 && j >= 0 && i < grid.size() && j < grid[i].size() && (grid[i][j] == '1')) {
-		grid[i][j] = '2';
+		grid[i][j] = '2';//or '0', just not '1'
 		search(grid, i + 1, j);
 		search(grid, i - 1, j);
 		search(grid, i, j + 1);
 		search(grid, i, j - 1);
 	}
 }
+//not fastest, but easy
