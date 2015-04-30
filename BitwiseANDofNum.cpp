5bd6a276cde05b9d2074ea7a5333c665b606a9bf
Bread <chisbread@gmail.com>
2015-05-01 00:32:24 +0800

    Update BitwiseANDofNum.cpp

--- BitwiseANDofNum.cpp
+++ BitwiseANDofNum.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
 	int rangeBitwiseAnd(int m, int n) {
-		if(!(m && n)) {//(0 & x = 0)
-			return 0;
-		}
-		unsigned int ans = 1 << 31;
-		while(!((ans = ans >> 1) & n));//get biggest bit
-		if(ans & m) {//is match?
-			ans += rangeBitwiseAnd(m - ans, n - ans);//subbitsqc
-		}
-		else {//not match (0111 &to 1111 = 0)
-			ans = 0;
+		unsigned int temp(m^n), temp2(1 << 31), ans = 0;
+		while(((temp2 = temp2 >> 1) > temp)) {//until first '!=bits'
+			if(temp2 & n) {
+				ans += temp2;
+			}
 		}
 		return ans;
 	}
 };
-//is it fast?
